cece2f76e7b2f826661c278dcdba1e4a99b241b8
Chia-Sheng Chen <mail@cschen.io>
2022-02-08 01:49:11 +0800

    I am a boy this is a pen

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import * as eva from '@eva-design/eva';
+import {
+    ApplicationProvider,
+    Layout,
+    Text,
+    Card,
+    Divider,
+    List,
+    ListItem
+} from '@ui-kitten/components';
+import { default as theme } from './theme.json';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hi Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const HomeScreen = () => (
+  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text category='h1' status='primary'>HOME</Text>
+    <Text category='h2'>
+        Hi
+    </Text>
+    <Text>
+        I am a boy. This is a pen.
+    </Text>
+    <ListDividersShowcase/>
+  </Layout>
+);
+
+const data = new Array(8).fill({
+    title: 'Item',
+    description: 'Description for Item',
+});
+
+const ListDividersShowcase = () => {
+
+    const renderItem = ({ item, index }) => (
+      <ListItem
+        title={`${item.title} ${index + 1}`}
+        description={`${item.description} ${index + 1}`}
+      />
+    );
+
+    return (
+      <List
+        style={styles.container}
+        data={data}
+        ItemSeparatorComponent={Divider}
+        renderItem={renderItem}
+      />
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+      width: 400,
+      maxHeight: 200,
+    },
 });
+
+export default () => (
+  <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
+    <HomeScreen />
+  </ApplicationProvider>
+);
