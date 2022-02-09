4236a2d0a407eb855fcb0e2f4e7a97f53a996485
Chia-Sheng Chen <mail@cschen.io>
2022-02-09 18:31:37 +0800

    Plurk ui-kitten

--- src/pages/chartPage.js
+++ src/pages/chartPage.js
@@ -1,14 +1,5 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import {
-    ApplicationProvider,
-    Layout,
-    Text,
-    Card,
-    Divider,
-    List,
-    ListItem
-} from '@ui-kitten/components';
+import { StyleSheet, Text } from 'react-native';
 
 const ChartPage = () => {
   return <Text>chart</Text>;
