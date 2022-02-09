e87e705012b8246b820e662140b0376befc6a474
Chia-Sheng Chen <mail@cschen.io>
2022-02-09 20:42:27 +0800

    Finish page buttons layout prototype

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View, Image } from 'react-native';
 
 import InputPage from "pages/inputPage";
 import ChartPage from "pages/chartPage";
 import DataPage from "pages/dataPage";
+import Button from "components/button";
 
 const HomeScreen = () => {
   const [page, setPage] = React.useState("Input");
 
   return (
   <View style={styles.base}>
+    <Image resizeMode="cover" style={styles.heroImg} source={require("@assets/hero.jpg")}></Image>
     <View style={styles.container}>
       <View style={styles.buttonGroup}>
-        <Button style={styles.button} title="Input" onPress={() => setPage("Input")}/>
-        <Button style={styles.button} title="Chart" onPress={() => setPage("Chart")}/>
-        <Button style={styles.button} title="Data" onPress={() => setPage("Data")}/>
+        <Button title="Input" icon="flash-outline" onPress={() => setPage("Input")}/>
+        <Button title="Chart" icon="activity-outline" onPress={() => setPage("Chart")}/>
+        <Button title="Data" icon="layers-outline" onPress={() => setPage("Data")}/>
       </View>
+    </View>
+    <View style={styles.container}>
       { page == "Input" && <InputPage/> }
       { page == "Chart" && <ChartPage/> }
       { page == "Data" && <DataPage/> }
@@ -29,24 +33,25 @@ export default () => (
 );
 
 const styles = {
-  "base": {
-    "position": "absolute",
-    "top": 0,
-    "bottom": 0,
-    "left": 0,
-    "right": 0,
-    "padding": "2%",
-    "background-color": "#333",
+  heroImg: {
+    width: "100%",
+    height: 100,
   },
-  "container": {
-    "position": "absolute",
-    "top": "10%",
+  base: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
   },
-  "buttonGroup": {
-    "flex-direction": "row",
-    "flex-wrap": "wrap"
+  container: {
+    paddingTop: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
   },
-  "button": {
-    "margin-left": "2px",
-  }
+  buttonGroup: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+  },
 };
