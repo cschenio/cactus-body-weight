32d79831424ec26c9d2af6500cd3e3e970b1f6df
shyao_book3 <shyao@microsoft.com>
2022-02-09 18:35:44 +0800

    create FloatInput()

--- src/pages/inputPage.js
+++ src/pages/inputPage.js
@@ -1,8 +1,42 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, TextInput, View} from 'react-native';
 
 const InputPage = () => {
-  return <Text>input</Text>;
+  return (
+    <View style={styles.container}>
+      <Text>input</Text>
+      <FloatingInput/>
+    </View>
+  );
 }
+
+const FloatingInput = () => {
+  const [number, onChangeNumber] = React.useState(null);
+  
+  return (
+    <TextInput
+      style = {styles.floatingInput}
+      onChangeText={onChangeNumber}
+      value = {`${number}`}
+      placeholder = "0.0"
+      keyboardType = "numeric"
+      placeholderTextColor = "#000"
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  floatingInput: {
+    height: 40,
+    width: "90%",
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+  },
+  container:{
+    backgroundColor: "#0a5a7ac6",
+  }
+});
 
 export default InputPage;
