ecadc8409d80b75f59436e95836dab872b7bd094
shyao_book3 <shyao@microsoft.com>
2022-02-09 20:30:20 +0800

    Complete simple input-floating-box without style and the footer button

--- src/pages/inputPage.js
+++ src/pages/inputPage.js
@@ -1,41 +1,77 @@
-import React from 'react';
-import { StyleSheet, Text, TextInput, View} from 'react-native';
+import React, {useRef} from 'react';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+
 
 const InputPage = () => {
+  const refWeight = useRef();
+  const refFat = useRef();
   return (
     <View style={styles.container}>
       <Text>input</Text>
-      <FloatingInput/>
+      <FloatingBox name="Weight:" suffix="kg" refMain={refWeight} refSubmit={refFat}/>
+      <FloatingBox name="Fat:" suffix="%" refMain={refFat} refSubmit={null}/>
     </View>
   );
 }
 
-const FloatingInput = () => {
-  const [number, onChangeNumber] = React.useState(null);
-  
+const InputPageFotter = () => {
   return (
-    <TextInput
-      style = {styles.floatingInput}
-      onChangeText={onChangeNumber}
-      value = {`${number}`}
-      placeholder = "0.0"
-      keyboardType = "numeric"
-      placeholderTextColor = "#000"
-    />
+    <Button style={styles.button} title="Add" />
+  )
+}
+
+const FloatingBox = (pros) => {
+  const [number, onChangeNumber] = React.useState(null);
+  const [isActive, setActive] = React.useState(false);
+
+  return (
+    <View style = {isActive?styles.boxContainerFocus:styles.boxContainerBlur} >
+      <Text style = {styles.floatingText}>
+        {pros.name}  
+      </Text>
+      <TextInput
+        style = {styles.floatingInput}
+        // value
+        onChangeText={onChangeNumber}
+        value = {number}
+        placeholder = "0.0"
+        keyboardType = "numeric"
+        placeholderTextColor = "#61a0f8"
+        maxLength = {5}
+        // reference
+        onSubmitEditing = {pros.refSubmit?() => pros.refSubmit.current.focus():null}
+        returnKeyType={Platform.OS === 'ios' ? 'done' : 'next'}
+        ref = {pros.refMain}
+        // focus style
+        onFocus={() => setActive(true)} 
+        onBlur={() => setActive(false)}
+      />
+      <Text style = {styles.floatingText}>
+        {pros.suffix}  
+      </Text>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   floatingInput: {
-    height: 40,
-    width: "90%",
-    margin: 12,
+  },
+  floatingText:{
+  },
+  boxContainerFocus:{
+    margin: 0,
     borderWidth: 1,
     borderRadius: 10,
     padding: 10,
   },
-  container:{
+  boxContainerBlur:{
+    margin: 0,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
     backgroundColor: "#0a5a7ac6",
+  },
+  container:{
   }
 });
 
