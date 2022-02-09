04f261b9df518b7f6fa40f9c0d34cb54f7ce590b
shyao_book3 <shyao@microsoft.com>
2022-02-09 21:46:19 +0800

    add styles to floating-boxes and buttons

--- src/pages/inputPage.js
+++ src/pages/inputPage.js
@@ -1,31 +1,46 @@
 import React, {useRef} from 'react';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import * as Theme from "@assets/theme.json";
+import {StyleSheet, Text, TextInput, View, ScrollView, Pressable} from 'react-native';
+import Button from "components/button";
+import DatePicker from 'react-native-date-picker'
 
 
 const InputPage = () => {
   const refWeight = useRef();
   const refFat = useRef();
   return (
-    <View style={styles.container}>
-      <Text>input</Text>
-      <FloatingBox name="Weight:" suffix="kg" refMain={refWeight} refSubmit={refFat}/>
-      <FloatingBox name="Fat:" suffix="%" refMain={refFat} refSubmit={null}/>
-    </View>
+    <ScrollView style={styles.container}>
+      <DateBox name="Date:"/>
+      <FloatingBox name="Weight:" suffix="kg" maxNum="200" refMain={refWeight} refSubmit={refFat}/>
+      <FloatingBox name="Fat:" suffix="%" maxNum="100" refMain={refFat} refSubmit={null}/>
+      <InputPageFotter />
+    </ScrollView>
   );
 }
 
 const InputPageFotter = () => {
   return (
-    <Button style={styles.button} title="Add" />
+    <View style={styles.buttonGroup}>
+      <Button title="Done" icon="done-all-outline"/>
+      <Button title="Add" icon="plus-square-outline"/>
+    </View>
   )
 }
+
+const DateBox = (pros) => {
+  return (
+    <View>
+
+    </View>
+  );
+};
 
 const FloatingBox = (pros) => {
   const [number, onChangeNumber] = React.useState(null);
   const [isActive, setActive] = React.useState(false);
 
   return (
-    <View style = {isActive?styles.boxContainerFocus:styles.boxContainerBlur} >
+    <Pressable style = {isActive?styles.boxContainerFocus:styles.boxContainerBlur} onPress={()=>{pros.refMain.current.focus()}}>
       <Text style = {styles.floatingText}>
         {pros.name}  
       </Text>
@@ -33,10 +48,10 @@ const FloatingBox = (pros) => {
         style = {styles.floatingInput}
         // value
         onChangeText={onChangeNumber}
-        value = {number}
+        value = {parseFloat(number)>parseFloat(pros.maxNum)?pros.maxNum:number}
         placeholder = "0.0"
         keyboardType = "numeric"
-        placeholderTextColor = "#61a0f8"
+        // placeholderTextColor = Theme["color-info-300"],
         maxLength = {5}
         // reference
         onSubmitEditing = {pros.refSubmit?() => pros.refSubmit.current.focus():null}
@@ -49,30 +64,49 @@ const FloatingBox = (pros) => {
       <Text style = {styles.floatingText}>
         {pros.suffix}  
       </Text>
-    </View>
+    </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
   floatingInput: {
+    paddingHorizontal: 12,
+    paddingVertical: 12,
+    fontSize: 20,
+    width: "60%",
+    fontWeight: "bold",
   },
   floatingText:{
+    paddingHorizontal: 2,
+    paddingVertical: 12,
+    fontSize: 18,
+    width: "20%",
+    textAlign: "center",
   },
   boxContainerFocus:{
-    margin: 0,
-    borderWidth: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    margin: 10,
     borderRadius: 10,
     padding: 10,
+    backgroundColor: Theme["color-info-400"],
   },
   boxContainerBlur:{
-    margin: 0,
-    borderWidth: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    margin: 10,
     borderRadius: 10,
     padding: 10,
-    backgroundColor: "#0a5a7ac6",
+    backgroundColor: Theme["color-info-200"],
   },
   container:{
-  }
+    height: "100%",
+  },
+  buttonGroup: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+  },
 });
 
 export default InputPage;
